3d2f5d23daff5d6fcfabfc63334a8ee98ace6229
Rachel4858 <rachel458458@gmail.com>
2020-09-14 15:51:28 +0900

    [test]Test first commit

---
+++ src/leetcode/HappyNumber.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        HappyNumber happyNumber = new HappyNumber();
+
+        boolean isHappyResult = happyNumber.isHappy(19);
+        System.out.println(isHappyResult);
+    }
+
+
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        while (!set.contains(n)) {
+            set.add(n);
+            n = rotateSumN(n);
+
+            if (n == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int rotateSumN(int number) {
+        String tmpStr = Integer.toString(number);
+        String[] splittedNumArray = tmpStr.split("");
+        int tmpNum = 0;
+
+        for (int i = 0; i < splittedNumArray.length; i++) {
+            int strToInt = Integer.parseInt(splittedNumArray[i]);
+            tmpNum += strToInt * strToInt;
+        }
+
+        return tmpNum;
+    }
+}
